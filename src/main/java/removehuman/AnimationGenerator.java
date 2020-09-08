e1f62c5eb65f57910c6aacc064895acbf8fabb62
Tony Kwok <tonykwok1992@gmail.com>
2020-09-08 16:20:18 +0800

    error handling

--- src/main/java/removehuman/AnimationGenerator.java
+++ src/main/java/removehuman/AnimationGenerator.java
@@ -6,17 +6,18 @@ import org.bytedeco.opencv.opencv_core.Mat;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
 
 public class AnimationGenerator {
 
     private int counter = 0;
 
     public void recordFrame(Mat img) {
+
         try {
             ImageIO.write(Java2DFrameUtils.toBufferedImage(img), "jpg", new File("/tmp/image" + String.format("%04d" , counter++) + ".jpg"));
         } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            System.err.println("Error when writing frame for animation.");
+            e.printStackTrace();
         }
     }
 
